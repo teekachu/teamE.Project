05ffa34c242be3df49806f254a4adef68da5f387
Milo Wyner <milowyner@gmail.com>
2021-01-16 12:39:11 -0800

    Add CardDetailView.swift file

---
+++ ARKitProject/ARKitProject/Views/CardDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  CardDetailsView.swift
+//  ARKitProject
+//
+//  Created by Milo Wyner on 1/16/21.
+//
+
+import SwiftUI
+
+struct CardDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardDetailsView()
+    }
+}
