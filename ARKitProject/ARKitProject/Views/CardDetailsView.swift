9a431048ed579b1f2730c35b832a89dd8f39bfc0
Myat Thu Ko <nymyatthuko@gmail.com>
2021-01-17 14:13:41 -0500

    Removed white spaces and added large font on student

--- ARKitProject/ARKitProject/Views/CardDetailsView.swift
+++ ARKitProject/ARKitProject/Views/CardDetailsView.swift
@@ -40,85 +40,85 @@ struct CardDetailsView: View {
                 Rectangle()
                     .opacity(0.0)
                     .frame(height: 30)
-            
-            ZStack {
-                HStack {
-                    Image(person.imageName)
-                        .resizable()
-                        .clipShape(Circle())
-                        .frame(width: 300, height: 300)
-                        .padding(.leading, 100)
-                    
-                    Spacer()
-                    
-                    VStack(alignment: .leading) {
-                        if person.detail.occupation == "Student" {
-                            Text(person.detail.occupation)
-                            
-                            if let school = person.detail.school {
-                                HStackView(image: "studentdesk", type: school)
-                            }
-                        } else {
-                            HStack {
-                                Text(person.detail.occupation)
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                            }
-                            if let company = person.detail.company {
-                                HStackView(image: "building.2", type: company)
-                            }
-                        }
-                        HStackView(image: "iphone", type: person.detail.phoneNumber)
-                        HStackView(image: "envelope", type: person.detail.email)
-                    }
-                    .font(.system(size: 22))
-                    .foregroundColor(.white)
-                    .frame(width: 450, alignment: .leading)
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 25.0).opacity(0.4))
-                    .padding(.trailing, 40)
-                    //.padding(.trailing, 80)
-                }
-            }.foregroundColor(.black)
-            
-            if let website = URL(string: person.website) {
-                HStack {
-                    Spacer()
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(Color.blue)
-                        .frame(width: 480, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .overlay(
-                            HStack {
-                                Image(systemName: "link.circle.fill")
-                                    .font(.largeTitle)
-                                Link("Visit Website",
-                                     destination: website)
-                                    .font(.title)
-                            }
-                            .foregroundColor(.white)
-                            .padding()
-                    )
-                        .padding(.trailing, 40)
-                }
                 
-            }
+                ZStack {
+                    HStack {
+                        Image(person.imageName)
+                            .resizable()
+                            .clipShape(Circle())
+                            .frame(width: 300, height: 300)
+                            .padding(.leading, 100)
+                        
+                        Spacer()
+                        
+                        VStack(alignment: .leading) {
+                            if person.detail.occupation == "Student" {
+                                Text(person.detail.occupation)
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                
+                                if let school = person.detail.school {
+                                    HStackView(image: "studentdesk", type: school)
+                                }
+                            } else {
+                                HStack {
+                                    Text(person.detail.occupation)
+                                        .font(.largeTitle)
+                                        .fontWeight(.bold)
+                                }
+                                if let company = person.detail.company {
+                                    HStackView(image: "building.2", type: company)
+                                }
+                            }
+                            HStackView(image: "iphone", type: person.detail.phoneNumber)
+                            HStackView(image: "envelope", type: person.detail.email)
+                        }
+                        .font(.system(size: 22))
+                        .foregroundColor(.white)
+                        .frame(width: 450, alignment: .leading)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 25.0).opacity(0.4))
+                        .padding(.trailing, 40)
+                        //.padding(.trailing, 80)
+                    }
+                }.foregroundColor(.black)
+                
+                if let website = URL(string: person.website) {
+                    HStack {
+                        Spacer()
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color.blue)
+                            .frame(width: 480, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .overlay(
+                                HStack {
+                                    Image(systemName: "link.circle.fill")
+                                        .font(.largeTitle)
+                                    Link("Visit Website",
+                                         destination: website)
+                                        .font(.title)
+                                }
+                                .foregroundColor(.white)
+                                .padding()
+                            )
+                            .padding(.trailing, 40)
+                    }
+                    
+                }
                 
                 Rectangle()
                     .opacity(0.0)
                     .frame(height: 30)
-                
             }
-            
             
             Text("© cardAGAIN 2021")
                 .font(.title)
                 .foregroundColor(.white)
                 .padding()
-
+            
         }
         .background(LinearGradient(gradient: Gradient(colors: [.purple, .purple, .orange, .yellow, .white]), startPoint: .bottomLeading, endPoint: .topTrailing))
         .cornerRadius(30)
-
+        
     }
     
     struct CardDetailsView_Previews: PreviewProvider {
